34f53d067eeb5614d9272ce40f2c1e1a71d031c7
Dominik Cubelic <dominik.cubelic@microblink.com>
2020-06-10 11:54:50 +0200

    add sample creation script

---
+++ demoApp/main.dart
@@ -0,0 +1,159 @@
+import 'package:blinkid_flutter/blinkid_flutter.dart';
+import 'package:flutter/material.dart';
+import "dart:convert";
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  String _resultString = "";
+  String _fullDocumentFrontImageBase64 = "";
+  String _fullDocumentBackImageBase64 = "";
+  String _faceImageBase64 = "";
+
+  Future<void> scan() async {
+    String license;
+    if (Theme.of(context).platform == TargetPlatform.iOS) {
+      license = "sRwAAAEgY29tLmV4YW1wbGUuYmxpbmtpZEZsdXR0ZXJTYW1wbGWRQPR97CHKqmlpT8RNJqMsAVnYLfWLLi7+z4LMKBOsULCGMcifWys1es90iyDGM67IIiOwcFMNY5kU3R0s0utf5Ivb7bOmwdVOYWKJeCb0n0fsfNfNDaKZo+3+K29fgFBer4O5KddiFrvPe9DrZKTsqRtTfCfwjHdstlgPgGZ17IF5DOHe2CyWKuXCyawAVMgkVixIBGrxKi4KxB6vcJc6mM1NDpClCPa2d2Yamh+H0zydS60V8o4iNpwT/i67Cs4B2CclLhg0i0s=";
+    } else if (Theme.of(context).platform == TargetPlatform.android) {
+      license = "";
+    }
+
+    var idRecognizer = BlinkIdCombinedRecognizer();
+    idRecognizer.returnFullDocumentImage = true;
+    idRecognizer.returnFaceImage = true;
+
+    BlinkIdOverlaySettings settings = BlinkIdOverlaySettings();
+
+    var results = await BlinkIDFlutter.scanWithCamera(RecognizerCollection([idRecognizer]), settings, license);
+
+    if (!mounted) return;
+
+    if (results.length == 0) return;
+    if (results[0] is BlinkIdCombinedRecognizerResult) {
+      BlinkIdCombinedRecognizerResult result = results[0];
+
+      if (result.mrzResult.documentType == MrtdDocumentType.Passport) {
+        _resultString = getPassportResultString(result);
+      } else if (result.mrzResult.documentType == MrtdDocumentType.IdentityCard) {
+        _resultString = getIdResultString(result);
+      }
+
+      setState(() {
+        _resultString = _resultString;
+        _fullDocumentFrontImageBase64 = result.fullDocumentFrontImage;
+        _fullDocumentBackImageBase64 = result.fullDocumentBackImage;
+        _faceImageBase64 = result.faceImage;
+      });
+    }
+  }
+
+  String getIdResultString(BlinkIdCombinedRecognizerResult result) {
+    return
+      "First name: ${result.firstName}\n"
+      "Last name: ${result.lastName}\n"
+      "Address: ${result.address}\n"
+      "Document number: ${result.documentNumber}\n"
+      "Sex: ${result.sex}\n"
+      "Date of birth: ${result.dateOfBirth.day}."
+        "${result.dateOfBirth.month}."
+        "${result.dateOfBirth.year}\n"
+      "Date of issue: ${result.dateOfIssue.day}."
+        "${result.dateOfIssue.month}."
+        "${result.dateOfIssue.year}\n"
+      "Date of expiry: ${result.dateOfExpiry.day}."
+        "${result.dateOfExpiry.month}."
+        "${result.dateOfExpiry.year}\n";
+  }
+
+  String getPassportResultString(BlinkIdCombinedRecognizerResult result) {
+    return
+      "First name: ${result.mrzResult.secondaryId}\n"
+      "Last name: ${result.mrzResult.primaryId}\n"
+      "Document number: ${result.mrzResult.documentNumber}\n"
+      "Sex: ${result.mrzResult.gender}\n"
+      "Date of birth: ${result.mrzResult.dateOfBirth.day}."
+        "${result.mrzResult.dateOfBirth.month}."
+        "${result.mrzResult.dateOfBirth.year}\n"
+      "Date of expiry: ${result.mrzResult.dateOfExpiry.day}."
+        "${result.mrzResult.dateOfExpiry.month}."
+        "${result.mrzResult.dateOfExpiry.year}\n";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget fullDocumentFrontImage = Container();
+    if (_fullDocumentFrontImageBase64 != null &&
+        _fullDocumentFrontImageBase64 != "") {
+      fullDocumentFrontImage = Column(
+        children: <Widget>[
+          Text("Document Front Image:"),
+          Image.memory(
+            Base64Decoder().convert(_fullDocumentFrontImageBase64),
+            height: 180,
+            width: 350,
+          )
+        ],
+      );
+    }
+
+    Widget fullDocumentBackImage = Container();
+    if (_fullDocumentBackImageBase64 != null &&
+        _fullDocumentBackImageBase64 != "") {
+      fullDocumentBackImage = Column(
+        children: <Widget>[
+          Text("Document Back Image:"),
+          Image.memory(
+            Base64Decoder().convert(_fullDocumentBackImageBase64),
+            height: 180,
+            width: 350,
+          )
+        ],
+      );
+    }
+
+    Widget faceImage = Container();
+    if (_faceImageBase64 != null && _faceImageBase64 != "") {
+      faceImage = Column(
+        children: <Widget>[
+          Text("Face Image:"),
+          Image.memory(
+            Base64Decoder().convert(_faceImageBase64),
+            height: 150,
+            width: 100,
+          )
+        ],
+      );
+    }
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("BlinkID Sample"),
+        ),
+        body: SingleChildScrollView(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                    child: RaisedButton(
+                      child: Text("Scan"),
+                      onPressed: () => scan(),
+                    ),
+                    padding: EdgeInsets.only(bottom: 16.0)),
+                Text(_resultString),
+                fullDocumentFrontImage,
+                fullDocumentBackImage,
+                faceImage,
+              ],
+            )),
+      ));
+  }
+}
---
+++ initFlutterDemo.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+blink_id_plugin_path=`pwd`/BlinkID
+appName=sample
+
+# remove any existing code
+rm -rf $appName
+
+# create a sample application
+flutter create --org com.microblink $appName
+
+# enter into demo project folder
+pushd $appName
+
+IS_LOCAL_BUILD=true
+if [ ! "$IS_LOCAL_BUILD" = true ]; then
+  # add blinkid_flutter dependency to pubspec.yaml
+  perl -i~ -pe "BEGIN{$/ = undef;} s/dependencies:\n  flutter:\n    sdk: flutter/dependencies:\n  flutter:\n    sdk: flutter\n  blinkid_flutter:/" pubspec.yaml
+  echo "Using blinkid_flutter from flutter pub"
+else
+  # add blinkid_flutter dependency with local path to pubspec.yaml
+  perl -i~ -pe "BEGIN{$/ = undef;} s/dependencies:\n  flutter:\n    sdk: flutter/dependencies:\n  flutter:\n    sdk: flutter\n  blinkid_flutter:\n    path: ..\/BlinkID/" pubspec.yaml
+  echo "Using blinkid_flutter from this repo instead from flutter pub"
+fi
+
+flutter pub get
+
+# enter into android project folder
+pushd android
+
+# TODO: android specifics
+
+popd
+
+# enter into ios project folder
+pushd ios
+
+# install pod
+pod install
+
+if [ "$IS_LOCAL_BUILD" = true ]; then
+  echo "Replace pod with custom dev version of BlinkID framework"
+
+  pushd Pods/PPBlinkID
+  # rm -rf Microblink.bundle
+  # rm -rf Microblink.framework
+
+  # cp -r ~/Downloads/blinkid-ios/Microblink.bundle ./
+  # cp -r ~/Downloads/blinkid-ios/Microblink.framework ./
+  popd
+fi
+
+# go to react native root project
+popd
+
+cp ../demoAPP/main.dart lib/
+
+echo "Go to Flutter project folder: cd $appName"
+echo "To run on Android execute: ???"
+echo "To run on iOS: go to $appName/ios and open Runner.xcworkspace; set your development team and add Privacy - Camera Usage Description key to Runner/Info.plist file and press run"
