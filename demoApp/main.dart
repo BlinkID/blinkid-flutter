c9d73073723230daccf5d4907019b656df25f5ea
sandra_kuzmic <kuzmic.sandra@microblink.com>
2020-06-18 10:59:31 +0200

    update demoApp/main.dart

--- demoApp/main.dart
+++ demoApp/main.dart
@@ -1,6 +1,8 @@
 import 'package:blinkid_flutter/blinkid_flutter.dart';
 import 'package:flutter/material.dart';
 import "dart:convert";
+import "dart:async";
+
 
 void main() {
   runApp(MyApp());
@@ -23,7 +25,7 @@ class _MyAppState extends State<MyApp> {
     if (Theme.of(context).platform == TargetPlatform.iOS) {
       license = "sRwAAAEVY29tLm1pY3JvYmxpbmsuc2FtcGxl1BIcP+dpSuS/38LVOsaLNXzaiMXsGsDI9HoCvvTwW0ELAiAcBZdB/nLLlJdH4YxsICG1tc+42mh1/syML14kCuZrvHHUiGx1lwGyZGtAa65HTgU85+aXJwt95XSSiuiLjPXw6ue37/oqp3oHtw0P2xXzUA+jX6czpIynFScNPEInu81f1ylj15sbHxhmy/hPEvHEBd26myGxhjpM/JHX8jmrvmT02/ouLUWSbixVELFClQMBZ96X8omCq8l6SzuZJdRTcSxmpuj+";
     } else if (Theme.of(context).platform == TargetPlatform.android) {
-      license = "";
+      license = "sRwAAAAVY29tLm1pY3JvYmxpbmsuc2FtcGxlU9kJdf5ZkGlTu9W3vzSlBMML22Z+WDQp2HDIHyeFJ2T6rQqyfa/9N/DcqB6plnlcQ/BRPipC0+ErjoVlqUsCKxV0SktW2v9Rrtdfupf8jMpgPeXzq4J4X84OQiTcxTeNSoVSHED+d8mZsuCRG2XV85s/bVvzqyTm9nCWg6JgVnIPO6EsVihmgNHSGinAoslPfSzxxijS4dzvQJtqgnm4UeUGmST4vcF7Kv7ocfVA2UNevpg2SQAXIkEzsf39mx2WvtDdRK55";
     }
 
     var idRecognizer = BlinkIdCombinedRecognizer();
@@ -42,7 +44,7 @@ class _MyAppState extends State<MyApp> {
 
       if (result.mrzResult.documentType == MrtdDocumentType.Passport) {
         _resultString = getPassportResultString(result);
-      } else if (result.mrzResult.documentType == MrtdDocumentType.IdentityCard) {
+      } else {
         _resultString = getIdResultString(result);
       }
 
