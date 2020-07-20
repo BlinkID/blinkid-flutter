b751e95b93d4e6b735dc27934efb6bf64e302a44
Dominik Cubelic <dominik.cubelic@microblink.com>
2020-07-20 13:06:59 +0200

    flutter format

--- BlinkID/lib/recognizers/passport_recognizer.dart
+++ BlinkID/lib/recognizers/passport_recognizer.dart
@@ -6,92 +6,92 @@ part 'passport_recognizer.g.dart';
 
 /// Result object for PassportRecognizer.
 class PassportRecognizerResult extends RecognizerResult {
-    
-    ///Digital signature of the recognition result. Available only if enabled with signResult property. 
-    String digitalSignature;
-    
-    ///Version of the digital signature. Available only if enabled with signResult property. 
-    int digitalSignatureVersion;
-    
-    ///face image from the document if enabled with returnFaceImage property. 
-    String faceImage;
-    
-    ///full document image if enabled with returnFullDocumentImage property. 
-    String fullDocumentImage;
-    
-    ///The data extracted from the machine readable zone. 
-    MrzResult mrzResult;
-    
-    PassportRecognizerResult(Map<String, dynamic> nativeResult): super(RecognizerResultState.values[nativeResult['resultState']]) {
-        
-        this.digitalSignature = nativeResult["digitalSignature"];
-        
-        this.digitalSignatureVersion = nativeResult["digitalSignatureVersion"];
-        
-        this.faceImage = nativeResult["faceImage"];
-        
-        this.fullDocumentImage = nativeResult["fullDocumentImage"];
-        
-        this.mrzResult = nativeResult["mrzResult"] != null ? MrzResult(Map<String, dynamic>.from(nativeResult["mrzResult"])) : null;
-        
-    }
-}
+  ///Digital signature of the recognition result. Available only if enabled with signResult property.
+  String digitalSignature;
 
+  ///Version of the digital signature. Available only if enabled with signResult property.
+  int digitalSignatureVersion;
+
+  ///face image from the document if enabled with returnFaceImage property.
+  String faceImage;
+
+  ///full document image if enabled with returnFullDocumentImage property.
+  String fullDocumentImage;
+
+  ///The data extracted from the machine readable zone.
+  MrzResult mrzResult;
+
+  PassportRecognizerResult(Map<String, dynamic> nativeResult)
+      : super(RecognizerResultState.values[nativeResult['resultState']]) {
+    this.digitalSignature = nativeResult["digitalSignature"];
+
+    this.digitalSignatureVersion = nativeResult["digitalSignatureVersion"];
+
+    this.faceImage = nativeResult["faceImage"];
+
+    this.fullDocumentImage = nativeResult["fullDocumentImage"];
+
+    this.mrzResult = nativeResult["mrzResult"] != null
+        ? MrzResult(Map<String, dynamic>.from(nativeResult["mrzResult"]))
+        : null;
+  }
+}
 
 ///Recognizer which can scan all passports with MRZ.
 @JsonSerializable()
 class PassportRecognizer extends Recognizer {
-    
-    ///Defines whether to anonymize Netherlands MRZ
-    /// 
-    /// 
-    bool anonymizeNetherlandsMrz = true;
-    
-    ///Defines if glare detection should be turned on/off.
-    /// 
-    /// 
-    bool detectGlare = true;
-    
-    ///Property for setting DPI for face images
-    /// Valid ranges are [100,400]. Setting DPI out of valid ranges throws an exception
-    /// 
-    /// 
-    int faceImageDpi = 250;
-    
-    ///Property for setting DPI for full document images
-    /// Valid ranges are [100,400]. Setting DPI out of valid ranges throws an exception
-    /// 
-    /// 
-    int fullDocumentImageDpi = 250;
-    
-    ///Image extension factors for full document image.
-    /// 
-    /// @see ImageExtensionFactors
-    /// 
-    ImageExtensionFactors fullDocumentImageExtensionFactors = ImageExtensionFactors();
-    
-    ///Sets whether face image from ID card should be extracted
-    /// 
-    /// 
-    bool returnFaceImage = false;
-    
-    ///Sets whether full document image of ID card should be extracted.
-    /// 
-    /// 
-    bool returnFullDocumentImage = false;
-    
-    ///Whether or not recognition result should be signed.
-    /// 
-    /// 
-    bool signResult = false;
-    
-    PassportRecognizer(): super('PassportRecognizer');
+  ///Defines whether to anonymize Netherlands MRZ
+  ///
+  ///
+  bool anonymizeNetherlandsMrz = true;
 
-    RecognizerResult createResultFromNative(Map<String, dynamic> nativeResult) {
-        return PassportRecognizerResult(nativeResult);
-    }
+  ///Defines if glare detection should be turned on/off.
+  ///
+  ///
+  bool detectGlare = true;
 
-    factory PassportRecognizer.fromJson(Map<String, dynamic> json) => _$PassportRecognizerFromJson(json);
+  ///Property for setting DPI for face images
+  /// Valid ranges are [100,400]. Setting DPI out of valid ranges throws an exception
+  ///
+  ///
+  int faceImageDpi = 250;
 
-    Map<String, dynamic> toJson() => _$PassportRecognizerToJson(this);
+  ///Property for setting DPI for full document images
+  /// Valid ranges are [100,400]. Setting DPI out of valid ranges throws an exception
+  ///
+  ///
+  int fullDocumentImageDpi = 250;
+
+  ///Image extension factors for full document image.
+  ///
+  /// @see ImageExtensionFactors
+  ///
+  ImageExtensionFactors fullDocumentImageExtensionFactors =
+      ImageExtensionFactors();
+
+  ///Sets whether face image from ID card should be extracted
+  ///
+  ///
+  bool returnFaceImage = false;
+
+  ///Sets whether full document image of ID card should be extracted.
+  ///
+  ///
+  bool returnFullDocumentImage = false;
+
+  ///Whether or not recognition result should be signed.
+  ///
+  ///
+  bool signResult = false;
+
+  PassportRecognizer() : super('PassportRecognizer');
+
+  RecognizerResult createResultFromNative(Map<String, dynamic> nativeResult) {
+    return PassportRecognizerResult(nativeResult);
+  }
+
+  factory PassportRecognizer.fromJson(Map<String, dynamic> json) =>
+      _$PassportRecognizerFromJson(json);
+
+  Map<String, dynamic> toJson() => _$PassportRecognizerToJson(this);
 }
--- BlinkID/lib/recognizers/usdl_combined_recognizer.dart
+++ BlinkID/lib/recognizers/usdl_combined_recognizer.dart
@@ -6,173 +6,180 @@ part 'usdl_combined_recognizer.g.dart';
 
 /// Result object for UsdlCombinedRecognizer.
 class UsdlCombinedRecognizerResult extends RecognizerResult {
+  /// Digital signature of the recognition result. Available only if enabled with signResult property.
+  String digitalSignature;
 
-        /// Digital signature of the recognition result. Available only if enabled with signResult property. 
-        String digitalSignature;
-        
-        /// Version of the digital signature. Available only if enabled with signResult property. 
-        int digitalSignatureVersion;
-        
-        /// Returns true if data from scanned parts/sides of the document match,
-        /// false otherwise. For example if date of expiry is scanned from the front and back side
-        /// of the document and values do not match, this method will return false. Result will
-        /// be true only if scanned values for all fields that are compared are the same. 
-        DataMatchResult documentDataMatch;
-        
-        /// face image from the document if enabled with returnFaceImage property. 
-        String faceImage;
-        
-        /// full document image if enabled with returnFullDocumentImage property. 
-        String fullDocumentImage;
-        
-        /// Returns true if recognizer has finished scanning first side and is now scanning back side,
-        /// false if it's still scanning first side. 
-        bool scanningFirstSideDone;
-        
-        /// Array of elements that are not part of AAMVA standard and are specific to each US state. 
-        List<String> optionalElements;
-    
-        /// The raw bytes contained inside 2D barcode. 
-        String rawData;
-    
-        /// Raw string inside 2D barcode. 
-        String rawStringData;
-    
-        /// True if returned result is uncertain, i.e. if scanned barcode was incomplete (i.e. 
-        bool uncertain;
+  /// Version of the digital signature. Available only if enabled with signResult property.
+  int digitalSignatureVersion;
 
-        /// Fields inside US Driver's licence. Available Keys are listed in UsdlKeys enum. 
-        List<String> fields;
+  /// Returns true if data from scanned parts/sides of the document match,
+  /// false otherwise. For example if date of expiry is scanned from the front and back side
+  /// of the document and values do not match, this method will return false. Result will
+  /// be true only if scanned values for all fields that are compared are the same.
+  DataMatchResult documentDataMatch;
 
-        /// The first name of the United States driver license owner. 
-        String firstName;
+  /// face image from the document if enabled with returnFaceImage property.
+  String faceImage;
 
-        /// The last name of the United States driver license owner. 
-        String lastName;
+  /// full document image if enabled with returnFullDocumentImage property.
+  String fullDocumentImage;
 
-        /// The full name of the United States driver license owner. 
-        String fullName;
+  /// Returns true if recognizer has finished scanning first side and is now scanning back side,
+  /// false if it's still scanning first side.
+  bool scanningFirstSideDone;
 
-        /// The full address of the United States driver license owner. 
-        String address;
+  /// Array of elements that are not part of AAMVA standard and are specific to each US state.
+  List<String> optionalElements;
 
-        /// The document number of the United States driver license. 
-        String documentNumber;
+  /// The raw bytes contained inside 2D barcode.
+  String rawData;
 
-        /// The sex of the United States driver license owner. 
-        String sex;
+  /// Raw string inside 2D barcode.
+  String rawStringData;
 
-        /// The restrictions to driving privileges for the United States driver license owner. 
-        String restrictions;
+  /// True if returned result is uncertain, i.e. if scanned barcode was incomplete (i.e.
+  bool uncertain;
 
-        /// The additional privileges granted to the United States driver license owner. 
-        String endorsements;
+  /// Fields inside US Driver's licence. Available Keys are listed in UsdlKeys enum.
+  List<String> fields;
 
-        /// The type of vehicle the driver license owner has privilege to drive. 
-        String vehicleClass;
+  /// The first name of the United States driver license owner.
+  String firstName;
 
-        /// The date of birth of the United States driver license owner. 
-        Date dateOfBirth;
+  /// The last name of the United States driver license owner.
+  String lastName;
 
-        /// The date of issue of the United States driver license. 
-        Date dateOfIssue;
+  /// The full name of the United States driver license owner.
+  String fullName;
 
-        /// The date of expiry of the United States driver license. 
-        Date dateOfExpiry;
+  /// The full address of the United States driver license owner.
+  String address;
 
-        /// The current age of the document owner in years. It is calculated difference
-        /// between now and date of birth. Now is current time on the device.
-        /// @return current age of the document owner in years or -1 if date of birth is unknown.
-        int age;
+  /// The document number of the United States driver license.
+  String documentNumber;
 
-    UsdlCombinedRecognizerResult(Map<String, dynamic> nativeResult): super(RecognizerResultState.values[nativeResult['resultState']]) {
+  /// The sex of the United States driver license owner.
+  String sex;
 
-        this.digitalSignature = nativeResult['digitalSignature'];
-        
-        this.digitalSignatureVersion = nativeResult['digitalSignatureVersion'];
-        
-        this.documentDataMatch = DataMatchResult.values[nativeResult["documentDataMatch"]];
+  /// The restrictions to driving privileges for the United States driver license owner.
+  String restrictions;
 
-        this.faceImage = nativeResult['faceImage'];
+  /// The additional privileges granted to the United States driver license owner.
+  String endorsements;
 
-        this.fullDocumentImage = nativeResult['fullDocumentImage'];
-        
-        this.scanningFirstSideDone = nativeResult['scanningFirstSideDone'];
-        
-        this.optionalElements = List<String>.from(nativeResult['optionalElements']);
-    
-        this.rawData = nativeResult['rawData'];
-    
-        this.rawStringData = nativeResult['rawStringData'];
-    
-        this.uncertain = nativeResult['uncertain'];
+  /// The type of vehicle the driver license owner has privilege to drive.
+  String vehicleClass;
 
-        this.fields = List<String>.from(nativeResult['fields']);
+  /// The date of birth of the United States driver license owner.
+  Date dateOfBirth;
 
-        this.firstName = nativeResult['firstName'];
+  /// The date of issue of the United States driver license.
+  Date dateOfIssue;
 
-        this.lastName = nativeResult['lastName'];
+  /// The date of expiry of the United States driver license.
+  Date dateOfExpiry;
 
-        this.fullName = nativeResult['fullName'];
+  /// The current age of the document owner in years. It is calculated difference
+  /// between now and date of birth. Now is current time on the device.
+  /// @return current age of the document owner in years or -1 if date of birth is unknown.
+  int age;
 
-        this.address = nativeResult['address'];
+  UsdlCombinedRecognizerResult(Map<String, dynamic> nativeResult)
+      : super(RecognizerResultState.values[nativeResult['resultState']]) {
+    this.digitalSignature = nativeResult['digitalSignature'];
 
-        this.documentNumber = nativeResult['documentNumber'];
+    this.digitalSignatureVersion = nativeResult['digitalSignatureVersion'];
 
-        this.sex = nativeResult['sex'];
+    this.documentDataMatch =
+        DataMatchResult.values[nativeResult["documentDataMatch"]];
 
-        this.restrictions = nativeResult['restrictions'];
+    this.faceImage = nativeResult['faceImage'];
 
-        this.endorsements = nativeResult['endorsements'];
+    this.fullDocumentImage = nativeResult['fullDocumentImage'];
 
-        this.vehicleClass = nativeResult['vehicleClass'];
+    this.scanningFirstSideDone = nativeResult['scanningFirstSideDone'];
 
-        this.dateOfBirth = nativeResult['dateOfBirth'] != null ? Date(nativeResult['dateOfBirth']) : null;
+    this.optionalElements = List<String>.from(nativeResult['optionalElements']);
 
-        this.dateOfIssue = nativeResult['dateOfIssue'] != null ? Date(nativeResult['dateOfIssue']) : null;
+    this.rawData = nativeResult['rawData'];
 
-        this.dateOfExpiry = nativeResult['dateOfExpiry'] != null ? Date(nativeResult['dateOfExpiry']) : null;
+    this.rawStringData = nativeResult['rawStringData'];
 
-        this.age = nativeResult['age'];
-    }
+    this.uncertain = nativeResult['uncertain'];
+
+    this.fields = List<String>.from(nativeResult['fields']);
+
+    this.firstName = nativeResult['firstName'];
+
+    this.lastName = nativeResult['lastName'];
+
+    this.fullName = nativeResult['fullName'];
+
+    this.address = nativeResult['address'];
+
+    this.documentNumber = nativeResult['documentNumber'];
+
+    this.sex = nativeResult['sex'];
+
+    this.restrictions = nativeResult['restrictions'];
+
+    this.endorsements = nativeResult['endorsements'];
+
+    this.vehicleClass = nativeResult['vehicleClass'];
+
+    this.dateOfBirth = nativeResult['dateOfBirth'] != null
+        ? Date(nativeResult['dateOfBirth'])
+        : null;
+
+    this.dateOfIssue = nativeResult['dateOfIssue'] != null
+        ? Date(nativeResult['dateOfIssue'])
+        : null;
+
+    this.dateOfExpiry = nativeResult['dateOfExpiry'] != null
+        ? Date(nativeResult['dateOfExpiry'])
+        : null;
+
+    this.age = nativeResult['age'];
+  }
 }
-
 
 ///USDL Combined Recognizer.
 ///
 ///USDL Combined recognizer is used for scanning both front and back side of US Driver's License.
 @JsonSerializable()
 class UsdlCombinedRecognizer extends Recognizer {
-    /// Property for setting DPI for face images
-    /// Valid ranges are [100,400]. Setting DPI out of valid ranges throws an exception
-    int faceImageDpi = 250;
-    
-    /// Property for setting DPI for full document images
-    /// Valid ranges are [100,400]. Setting DPI out of valid ranges throws an exception
-    int fullDocumentImageDpi = 250;
-    
-    /// Sets whether face image from ID card should be extracted
-    bool returnFaceImage = false;
-    
-    /// Sets whether full document image of ID card should be extracted.
-    bool returnFullDocumentImage = false;
+  /// Property for setting DPI for face images
+  /// Valid ranges are [100,400]. Setting DPI out of valid ranges throws an exception
+  int faceImageDpi = 250;
 
-    /// The extension factors for full document image. 
-    ImageExtensionFactors fullDocumentImageExtensionFactors = ImageExtensionFactors();
-    
-    /// Minimum number of stable detections required for detection to be successful. 
-    int numStableDetectionsThreshold = 6;
-    
-    /// Whether or not recognition result should be signed.
-    bool signResult = false;
+  /// Property for setting DPI for full document images
+  /// Valid ranges are [100,400]. Setting DPI out of valid ranges throws an exception
+  int fullDocumentImageDpi = 250;
 
-    UsdlCombinedRecognizer(): super('UsdlCombinedRecognizer');
+  /// Sets whether face image from ID card should be extracted
+  bool returnFaceImage = false;
 
-    RecognizerResult createResultFromNative(Map<String, dynamic> nativeResult) {
-        return UsdlCombinedRecognizerResult(nativeResult);
-    }
+  /// Sets whether full document image of ID card should be extracted.
+  bool returnFullDocumentImage = false;
 
-    factory UsdlCombinedRecognizer.fromJson(Map<String, dynamic> json) => _$UsdlCombinedRecognizerFromJson(json);
+  /// The extension factors for full document image.
+  ImageExtensionFactors fullDocumentImageExtensionFactors =
+      ImageExtensionFactors();
 
-    Map<String, dynamic> toJson() => _$UsdlCombinedRecognizerToJson(this);
+  /// Minimum number of stable detections required for detection to be successful.
+  int numStableDetectionsThreshold = 6;
+
+  /// Whether or not recognition result should be signed.
+  bool signResult = false;
+
+  UsdlCombinedRecognizer() : super('UsdlCombinedRecognizer');
+
+  RecognizerResult createResultFromNative(Map<String, dynamic> nativeResult) {
+    return UsdlCombinedRecognizerResult(nativeResult);
+  }
+
+  factory UsdlCombinedRecognizer.fromJson(Map<String, dynamic> json) =>
+      _$UsdlCombinedRecognizerFromJson(json);
+
+  Map<String, dynamic> toJson() => _$UsdlCombinedRecognizerToJson(this);
 }
