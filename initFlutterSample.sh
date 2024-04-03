91e10e85760455e4f8affd8bc127677fffbc19ae
mparadina <milan.paradina@microblink.com>
2024-04-03 16:06:14 +0200

    Update main.dart and init script

--- initFlutterSample.sh
+++ initFlutterSample.sh
@@ -14,11 +14,11 @@ pushd $appName
 IS_LOCAL_BUILD=false || exit 1
 if [ "$IS_LOCAL_BUILD" = true ]; then
   # add blinkid_flutter dependency with local path to pubspec.yaml
-  perl -i~ -pe "BEGIN{$/ = undef;} s/dependencies:\n  flutter:\n    sdk: flutter/dependencies:\n  flutter:\n    sdk: flutter\n  blinkid_flutter:\n    path: ..\/BlinkID/" pubspec.yaml
+  perl -i~ -pe "BEGIN{$/ = undef;} s/dependencies:\n  flutter:\n    sdk: flutter/dependencies:\n  flutter:\n    sdk: flutter\n  blinkid_flutter:\n    path: ..\/BlinkID\n  image_picker: ^0.8.4+1/" pubspec.yaml
   echo "Using blinkid_flutter from this repo instead from flutter pub"
 else
   # add blinkid_flutter dependency to pubspec.yaml
-  perl -i~ -pe "BEGIN{$/ = undef;} s/dependencies:\n  flutter:\n    sdk: flutter/dependencies:\n  flutter:\n    sdk: flutter\n  blinkid_flutter:/" pubspec.yaml
+    perl -i~ -pe "BEGIN{$/ = undef;} s/dependencies:\n  flutter:\n    sdk: flutter/dependencies:\n  flutter:\n    sdk: flutter\n  blinkid_flutter:\n    path: ..\/BlinkID\n  image_picker: ^0.8.4+1/" pubspec.yaml
   echo "Using blinkid_flutter from flutter pub"
 fi
 
@@ -67,4 +67,4 @@ sed -i '' 's#minSdkVersion flutter.minSdkVersion#minSdkVersion 21#g' ./android/a
 echo ""
 echo "Go to Flutter project folder: cd $appName"
 echo "To run on Android type: flutter run"
-echo "To run on iOS: open $appName/ios/Runner.xcworkspace; set your development team and add Privacy - Camera Usage Description key to Runner/Info.plist file and press run"
+echo "To run on iOS: open $appName/ios/Runner.xcworkspace; set your development team and add Privacy - Camera Usage Description & Privacy - Photo Library Usage Description keys to Runner/Info.plist file and press run"
