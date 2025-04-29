87bb9c2c31c0229d662bb098544b0c692bf39b4b
mparadina <milan.paradina@microblink.com>
2025-04-29 15:43:14 +0200

    improve sample application

--- sample_files/main.dart
+++ sample_files/main.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:blinkid_flutter/blinkid_result.dart';
 import 'package:blinkid_flutter/blinkid_settings.dart';
 import 'package:flutter/material.dart';
 import "dart:async";
@@ -8,6 +7,7 @@ import 'package:flutter/services.dart';
 import 'package:blinkid_flutter/blinkid_flutter.dart';
 import "dart:convert";
 import 'package:image_picker/image_picker.dart';
+import 'blinkid_result_builder.dart';
 
 void main() {
   runApp(MyApp());
@@ -29,6 +29,21 @@ class _MyAppState extends State<MyApp> {
   String sdkLicenseKey = "";
 
   final blinkIdPlugin = BlinkidFlutter();
+
+  @override
+  void initState() {
+    super.initState();
+
+    /// Add a valid license key, based on the platform
+    /// A valid license key can be obtained from the Microblink Developer Hub, here: https://developer.microblink.com
+    if (Platform.isAndroid) {
+      sdkLicenseKey =
+          "sRwCABVjb20ubWljcm9ibGluay5zYW1wbGUAbGV5SkRjbVZoZEdWa1QyNGlPakUzTkRRNE56UTBOVEEzTURrc0lrTnlaV0YwWldSR2IzSWlPaUkwT1RabFpEQXpaUzAwT0RBeExUUXpZV1F0WVRrMU5DMDBNemMyWlRObU9UTTVNR1FpZlE9PQFqJ4q5cbrifIIzl/hZUv6kE1yuVi9JtleDjJowLJ8dNiwAGk1UsePWt8CO8WUH/+i1YBKciSxRfqFkTiMpPsLKEGvQ1zVG+3HZ4C4PMTwSEgtFu7HayF1P6Z7Bsb0=";
+    } else if (Platform.isIOS) {
+      sdkLicenseKey =
+          "sRwCABVjb20ubWljcm9ibGluay5zYW1wbGUBbGV5SkRjbVZoZEdWa1QyNGlPakUzTkRReE1ETTFOell6TVRjc0lrTnlaV0YwWldSR2IzSWlPaUkwT1RabFpEQXpaUzAwT0RBeExUUXpZV1F0WVRrMU5DMDBNemMyWlRObU9UTTVNR1FpZlE9Pa1EwEJ3KLtMGjfmgRGOpUrMLWyfJe7tCCCLMuyeuP25X0o8dUrLuPg+7SIra6+iruDS6gp8ksWoJ6jr3E1by7OALtyBtgV2jj16iY8GZM6nDBl+gBjDjPZu7qxD3yo=";
+    }
+  }
 
   Future<void> performScan() async {
     try {
@@ -54,20 +69,20 @@ class _MyAppState extends State<MyApp> {
       /// Place the image settings in the scanning settings
       scanningSettings.croppedImageSettings = imageSettings;
 
-      final filter = DocumentFilter.country(Country.Croatia);
+      final filter = DocumentFilter.country(Country.croatia);
       final filterTwo = DocumentFilter.typeRegion(
-        DocumentType.Dl,
-        Region.California,
+        DocumentType.dl,
+        Region.california,
       );
 
       final documentRules = [
         DocumentRules([
-          DetailedFieldType(FieldType.Address, AlphabetType.Latin),
-          DetailedFieldType(FieldType.FirstName, AlphabetType.Latin),
+          DetailedFieldType(FieldType.address, AlphabetType.latin),
+          DetailedFieldType(FieldType.firstName, AlphabetType.latin),
         ], filter),
         DocumentRules([
-          DetailedFieldType(FieldType.Address, AlphabetType.Latin),
-          DetailedFieldType(FieldType.FirstName, AlphabetType.Latin),
+          DetailedFieldType(FieldType.address, AlphabetType.latin),
+          DetailedFieldType(FieldType.firstName, AlphabetType.latin),
         ], filterTwo),
       ];
 
@@ -75,13 +90,13 @@ class _MyAppState extends State<MyApp> {
 
       final additionalAnonSettings =
           DocumentAnonymizationSettings.withAllParameters(
-            [FieldType.FirstName, FieldType.Address],
+            [FieldType.firstName, FieldType.address],
             filter,
             DocumentNumberAnonymizationSettings.withAllParemeters(1, 2),
           );
       final additionalAnonSettingsTwo = DocumentAnonymizationSettings([
-        FieldType.BloodType,
-        FieldType.Address,
+        FieldType.bloodType,
+        FieldType.address,
       ]);
 
       scanningSettings.customDocumentAnonymizationSettings = [
@@ -93,12 +108,12 @@ class _MyAppState extends State<MyApp> {
       sessionSettings.scanningSettings = scanningSettings;
       final classFilter = ClassFilter();
       classFilter.includeDocuments = [
-        DocumentFilter.country(Country.Usa),
-        DocumentFilter.countryRegion(Country.Usa, Region.California),
+        DocumentFilter.country(Country.usa),
+        DocumentFilter.countryRegion(Country.usa, Region.california),
       ];
       classFilter.excludeDocuments = [
-        DocumentFilter.country(Country.Croatia),
-        DocumentFilter.typeRegion(DocumentType.Dl, Region.Texas),
+        DocumentFilter.country(Country.croatia),
+        DocumentFilter.typeRegion(DocumentType.dl, Region.texas),
       ];
 
       /// call the 'performScan' method and handle the results
@@ -107,7 +122,7 @@ class _MyAppState extends State<MyApp> {
           .then((result) {
             setState(() {
               if (result != null) {
-                resultString = getIdResultString(result);
+                resultString = BlinkIdResultBuilder.getIdResultString(result);
                 if (result.firstDocumentImage != null) {
                   firstDocumentImageBase64 = result.firstDocumentImage!;
                 }
@@ -196,7 +211,7 @@ class _MyAppState extends State<MyApp> {
           .then((result) {
             setState(() {
               if (result != null) {
-                resultString = getIdResultString(result);
+                resultString = BlinkIdResultBuilder.getIdResultString(result);
                 if (result.firstDocumentImage != null) {
                   firstDocumentImageBase64 = result.firstDocumentImage!;
                 }
@@ -275,7 +290,7 @@ class _MyAppState extends State<MyApp> {
           .then((result) {
             setState(() {
               if (result != null) {
-                resultString = getIdResultString(result);
+                resultString = BlinkIdResultBuilder.getIdResultString(result);
                 if (result.firstDocumentImage != null) {
                   firstDocumentImageBase64 = result.firstDocumentImage!;
                 }
@@ -311,220 +326,8 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
-  String getIdResultString(BlinkIdScanningResult? result) {
-    if (result != null) {
-      return "Recognition mode: ${result.recognitionMode?.name}\n" +
-          buildResult(result.firstName, "First name") +
-          buildResult(result.lastName, "Last name") +
-          buildResult(result.fullName, "Full name") +
-          buildResult(result.localizedName, "Localized name") +
-          buildResult(
-            result.additionalNameInformation,
-            "Additional name info",
-          ) +
-          buildResult(result.address, "Address") +
-          buildResult(
-            result.additionalAddressInformation,
-            "Additional address info",
-          ) +
-          buildResult(result.documentNumber, "Document number") +
-          buildResult(
-            result.documentAdditionalNumber,
-            "Additional document number",
-          ) +
-          buildResult(result.sex, "Sex") +
-          buildResult(result.issuingAuthority, "Issuing authority") +
-          buildResult(result.nationality, "Nationality") +
-          buildDateResult(result.dateOfBirth, "Date of birth") +
-          buildDateResult(result.dateOfIssue, "Date of issue") +
-          buildDateResult(result.dateOfExpiry, "Date of expiry") +
-          buildResult(result.maritalStatus, "Martial status") +
-          buildResult(result.personalIdNumber, "Personal Id Number") +
-          buildResult(result.profession, "Profession") +
-          buildResult(result.race, "Race") +
-          buildResult(result.religion, "Religion") +
-          buildResult(result.residentialStatus, "Residential Status") +
-          buildDriverLicenceResult(result.driverLicenseDetailedInfo) +
-          buildDataMatchResult(result.dataMatchResult) +
-          buildDocumentClassInfoResult(result.documentClassInfo) +
-          buildSubresults(result.subResults);
-    }
-    return "";
-  }
-
-  String buildSubresults(List<SingleSideScanningResult>? subResults) {
-    String subResultsString = "";
-    if (subResults != null) {
-      for (var singleSideResult in subResults.asMap().entries) {
-        final index = singleSideResult.key;
-        final result = singleSideResult.value;
-
-        subResultsString += "\nDocument side ${index + 1} information:\n";
-        subResultsString += buildVizResult(result.viz);
-        subResultsString += buildMrzResult(result.mrz);
-        subResultsString += buildBarcodeResult(result.barcode);
-      }
-    }
-    return subResultsString;
-  }
-
-  String buildMrzResult(MrzResult? result) {
-    if (result == null) {
-      return "";
-    }
-
-    return "\nMRZ result:\n" +
-        buildStringResult(result.documentCode, "Document code") +
-        buildStringResult(result.documentNumber, "Document number") +
-        buildStringResult(result.gender, "Gender") +
-        buildStringResult(result.issuer, "Issuer") +
-        buildStringResult(result.issuerName, "Issuer name") +
-        buildStringResult(result.nationality, "Nationality") +
-        buildStringResult(result.nationalityName, "Nationality name") +
-        buildStringResult(result.opt1, "Opt1") +
-        buildStringResult(result.opt2, "Opt2") +
-        buildStringResult(result.primaryID, "Primary ID") +
-        buildStringResult(result.secondaryID, "Secondary ID") +
-        buildDateResult(result.dateOfBirth, "Date of birth") +
-        buildDateResult(result.dateOfExpiry, "Date of expiry");
-  }
-
-  String buildBarcodeResult(BarcodeResult? result) {
-    if (result == null) {
-      return "";
-    }
-    return "\nBarcode result:\n" +
-        buildStringResult(
-          result.additionalNameInformation,
-          "Additional name information",
-        ) +
-        buildStringResult(result.address, "Address") +
-        buildStringResult(
-          result.documentAdditionalNumber,
-          "Document additional number",
-        ) +
-        buildStringResult(result.documentNumber, "Document number") +
-        buildStringResult(result.employer, "Employer") +
-        buildStringResult(result.firstName, "First name") +
-        buildStringResult(result.lastName, "Last name") +
-        buildStringResult(result.fullName, "Full name") +
-        buildStringResult(result.issuingAuthority, "Issuing authority") +
-        buildStringResult(result.maritalStatus, "Marital status") +
-        buildStringResult(result.nationality, "Nationality") +
-        buildStringResult(result.personalIdNumber, "Personal ID number") +
-        buildDateResult(result.dateOfBirth, "Date of birth") +
-        buildDateResult(result.dateOfExpiry, "Date of expiry") +
-        buildDateResult(result.dateOfIssue, "Date of issue");
-  }
-
-  String buildVizResult(VizResult? result) {
-    if (result == null) {
-      return "";
-    }
-    return "\nVIZ result:\n" +
-        buildResult(
-          result.additionalAddressInformation,
-          "Additional address information",
-        ) +
-        buildResult(
-          result.additionalNameInformation,
-          "Additional name information",
-        ) +
-        buildResult(result.address, "Address") +
-        buildResult(result.bloodType, "Blood type") +
-        buildResult(result.documentNumber, "Document number") +
-        buildResult(result.documentSubtype, "Document subtype") +
-        buildResult(result.employer, "Employer") +
-        buildResult(result.firstName, "First name") +
-        buildResult(result.lastName, "Last name") +
-        buildResult(result.fullName, "Full name") +
-        buildResult(result.issuingAuthority, "Issuing authority") +
-        buildResult(result.maritalStatus, "Marital status") +
-        buildResult(result.nationality, "Nationality") +
-        buildResult(result.placeOfBirth, "Place of birth") +
-        buildDateResult(result.dateOfBirth, "Date of birth") +
-        buildDateResult(result.dateOfExpiry, "Date of expiry") +
-        buildDateResult(result.dateOfIssue, "Nationality");
-  }
-
-  String buildResult(StringResult? result, String propertyName) {
-    if (result == null || result.value == null || result!.value!.isEmpty) {
-      return "";
-    }
-
-    return "$propertyName: ${result.value!}\n";
-  }
-
-  String buildDocumentClassInfoResult(DocumentClassInfo? result) {
-    if (result == null) {
-      return "";
-    }
-
-    return "\nDocument class information:\nCountry: ${result.country?.name}\nRegion:${result.region?.name}\nDocument type: ${result.documentType?.name}\n";
-  }
-
-  String buildDateResult(DateResult? result, String propertyName) {
-    if (result == null || result!.date == null || result.date!.year == 0) {
-      return "";
-    }
-    if (result.originalString is StringResult) {
-      return buildResult(result.originalString, propertyName);
-    } else {
-      return buildStringResult(result.originalString, propertyName);
-    }
-  }
-
-  String buildStringResult(String? result, String propertyName) {
-    if (result == null || result.isEmpty) {
-      return "";
-    }
-
-    return "$propertyName: $result\n";
-  }
-
-  String buildIntResult(int? result, String propertyName) {
-    if (result == null || result < 0) {
-      return "";
-    }
-
-    return "$propertyName: $result\n";
-  }
-
-  String buildDriverLicenceResult(DriverLicenseDetailedInfo? result) {
-    if (result == null) {
-      return "";
-    }
-
-    return buildResult(result.restrictions, "Restrictions") +
-        buildResult(result.endorsements, "Endorsements") +
-        buildResult(result.vehicleClass, "Vehicle class") +
-        buildResult(result.conditions, "Conditions");
-  }
-
-  String buildDataMatchResult(DataMatchResult? dataMatchresult) {
-    if (dataMatchresult == null) {
-      return "";
-    }
-
-    String dataMatchResultString =
-        "\nData match information:\nState for whole document: ${dataMatchresult.overallState?.name}\n";
-
-    for (DataMatchResultField field in dataMatchresult!.states!) {
-      dataMatchResultString += "${field.field?.name}: ${field.state?.name}\n";
-    }
-    return dataMatchResultString;
-  }
-
   @override
   Widget build(BuildContext context) {
-    if (Platform.isAndroid) {
-      sdkLicenseKey =
-          "sRwCABVjb20ubWljcm9ibGluay5zYW1wbGUAbGV5SkRjbVZoZEdWa1QyNGlPakUzTkRVek9UVXpNelV3TWpnc0lrTnlaV0YwWldSR2IzSWlPaUprWkdRd05qWmxaaTAxT0RJekxUUXdNRGd0T1RRNE1DMDFORFU0WWpBeFlUVTJZamdpZlE9PT9ntCaTnqVQtm4xCnDsf6LGNiRcS+wJvDbVnmIzKk4hnXbAzz2S7i3rYBV2hgkyzChru7lZwjxNp8QppVtFK3lMMQxBRparnu1n/SFC23M1VxqQlE1xF2PYdUXhdA==";
-    } else if (Platform.isIOS) {
-      sdkLicenseKey =
-          "sRwCABVjb20ubWljcm9ibGluay5zYW1wbGUBbGV5SkRjbVZoZEdWa1QyNGlPakUzTkRVek9UVXpOekUwTmpNc0lrTnlaV0YwWldSR2IzSWlPaUprWkdRd05qWmxaaTAxT0RJekxUUXdNRGd0T1RRNE1DMDFORFU0WWpBeFlUVTJZamdpZlE9PeHKYUyGFbhY5Ro2y0K5nUW8aP44565wqKiAaN0IJjO4IA6/7tV+eAVUzWmGcyspxafJ4PagcjrdhJP9v38En5h/qsO7eAluRcVOgjNF0Ku4gxjy6BY/s3ffrdEJEw==";
-    }
-
     Widget firstDocumentImage = Container();
     if (firstDocumentImageBase64 != null && firstDocumentImageBase64 != "") {
       firstDocumentImage = Column(
@@ -662,9 +465,6 @@ class _MyAppState extends State<MyApp> {
           ),
         ),
       ),
-    ),
-  ),
-);
+    );
   }
 }
-
